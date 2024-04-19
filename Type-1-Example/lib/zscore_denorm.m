b1db38cfea70c27ee9329ab73b92e70cf51e0ca1
ata koklu <119673342+atakoklu@users.noreply.github.com>
2024-04-19 19:24:11 +0300

    Delete Type-1-Example directory

--- Type-1-Example/lib/zscore_denorm.m
+++
@@ -1,3 +0,0 @@
-function x_denorm = zscore_denorm(x_norm,mean,std)
-x_denorm = (x_norm.*std')+mean';
-end
--- Type-1-Example/lib/zscore_norm.m
+++
@@ -1,3 +0,0 @@
-function [x_norm,mean,std] = zscore_norm(x)
-[x_norm,mean,std] = zscore(x);
-end
